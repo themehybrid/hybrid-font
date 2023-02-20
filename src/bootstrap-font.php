b334f2edd319192e36f429c255cf7be9ad9622a2
saasfreelancer <syed.sayal.shah@gmail.com>
2023-02-20 20:38:02 +0500

    chore: linting
    - composer normalize
    - composer phpcbf

--- src/bootstrap-font.php
+++ src/bootstrap-font.php
@@ -10,12 +10,12 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-# Check if the package has been bootstrapped. If not, load the bootstrap files
-# and get the package set up.
+// Check if the package has been bootstrapped. If not, load the bootstrap files
+// and get the package set up.
 if ( ! defined( 'HYBRID_FONT_BOOTSTRAPPED' ) ) {
 
-	// Autoload functions.
-	require_once( __DIR__ . '/functions-helpers.php' );
+    // Autoload functions.
+    require_once __DIR__ . '/functions-helpers.php';
 
-	define( 'HYBRID_FONT_BOOTSTRAPPED', true );
+    define( 'HYBRID_FONT_BOOTSTRAPPED', true );
 }
